724b3c4f6c258ae1a907296d4dbbcb562869b99e
digitalbless <drive.digitalbless@gmail.com>
2023-12-28 11:24:52 -0300

    feat: framer motion server side renderizando novas paginas

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 import LoadMore from "../components/LoadMore";
-import { data } from "./_data";
 import { fetchAnime } from "./action";
 
 async function Home() {
@@ -13,9 +12,7 @@ async function Home() {
       </h2>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <LoadMore />
     </main>
